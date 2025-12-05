88219e19d20f6fc216e0b57eeea03a87cb7b07d8
DK <dhananjaykhirdikar3@gmail.com>
2025-12-05 15:06:25 +0530

    new

--- Datablock/main.tf
+++ Datablock/main.tf
@@ -41,7 +41,7 @@ data "aws_security_group" "data_webserver_sg" {
     name = "launch-wizard-1"  
 }
 data "aws_ami" "ami" {
-    name = al2023-ami-2023.9.20251117.1-kernel-6.1-x86_64
+    id = ami-0fa3fe0fa7920f68e
 }
 data "aws_instance" "data_webserver_id" {
     name = i-038bc2fed0e40815a   

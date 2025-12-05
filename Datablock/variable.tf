b2c2e901f682d682fcdec85e032803d9c9337832
DK <dhananjaykhirdikar3@gmail.com>
2025-12-05 15:10:06 +0530

    new

---
+++ Datablock/variable.tf
@@ -0,0 +1,18 @@
+variable "webserver_instance_type" {
+    default = "t3.small"
+}
+variable "webserver_ami" {
+    default = "ami-0f00d706c4a80fd93"
+}
+variable "webserver_key_name" {
+    default = "octa"
+}
+variable "webserver_instance_vpc_security_group_ids" {
+    default = "sg-0b4ace81d68dd0b6d"
+}
+variable "webserver_disable_api_termination" {
+    default = false
+}
+#variable "webserver_count" {
+ #   default = 1
+#}

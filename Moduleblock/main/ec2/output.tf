87970be6503aa689d91c90bdbc676aa09de6926a
Dhananjay Khirdikar <khirdikardhananjay2310@gmail.com>
2025-12-09 23:53:18 +0530

    push

--- Moduleblock/main/ec2/output.tf
+++
@@ -1,6 +0,0 @@
-output "web_public_ip" {
-    value = aws_instance.web.web_public_ip    
-}
-output "web_public_dns" {
-    value = aws_instance.web.web_public_dns
-}
--- Moduleblock/main/vpc/variable.tf
+++
@@ -1,6 +0,0 @@
-variable "vpc_web_cidr" {
-}
-variable "vpc_web_subnetA" {
-}
-variable "public_ip" {
-}

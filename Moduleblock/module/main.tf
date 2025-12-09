7fbf724ede585201c76e98f50d80761fc0b3cdd0
Dhananjay Khirdikar <khirdikardhananjay2310@gmail.com>
2025-12-09 23:04:12 +0530

    final

--- Moduleblock/module/main.tf
+++ Moduleblock/module/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "\mnt\C:\Users\DHANANJAY\Desktop\Terraform\Moduleblock\main\ec2"
+    source = "/home/dk/Terraform/Moduleblock/main/ec2"
     webserver_instance_type = "t3.micro"
     web_ami = "ami-02b8269d5e85954ef"
     web_key_name = "dk"
@@ -9,7 +9,7 @@ module "ec2" {
 }
 
 module "vpc" {
-    source = "\mnt\C:\Users\DHANANJAY\Desktop\Terraform\Moduleblock\main\vpc"
+    source = "/home/dk/Terraform/Moduleblock/main/vpc"
     vpc_web_cidr = "10.0.0.0/16"
     vpc_web_subnetA = "10.0.1.0/24"
     vpc_web_public_ip = true
